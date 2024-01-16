1f9ed3d644364c3f6aefeebbfdd862e8348d625e
JuanFeliciano <139025070+JuanFeliciano@users.noreply.github.com>
2024-01-16 10:18:16 -0300

    initial commit

---
+++ src/data-source.ts
@@ -0,0 +1,12 @@
+import { DataSource } from "typeorm";
+
+const port = process.env.DB_PORT as number | undefined;
+
+const AppDataSouce = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: port,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
